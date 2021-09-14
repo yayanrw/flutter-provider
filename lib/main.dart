2af163d580f4f52009f54200ee9efa3c03d2f906
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-14 21:03:20 +0700

    add main

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Provider State Management",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AnimatedContainer(
+                  margin: EdgeInsets.all(5),
+                  duration: Duration(milliseconds: 500),
+                  width: 150,
+                  height: 150,
+                  color: Colors.lightBlue,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(margin: EdgeInsets.all(5), child: Text("AB")),
+                    Switch(value: true, onChanged: (newValue) {}),
+                    Container(margin: EdgeInsets.all(5), child: Text("LB")),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
